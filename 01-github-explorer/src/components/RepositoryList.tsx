511a8520b10e32795323f9b1ec5be9a99842f07d
José Pedro <jgomes@geobit.com.br>
2021-10-30 22:02:24 -0300

    feat: adicionando projeto feito no capitulo I do ignite

---
+++ 01-github-explorer/src/components/RepositoryList.tsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import { RepositoryItem } from "./RepositoryItem";
+
+import '../styles/repositories.scss';
+
+
+interface Repository {
+    id: string;
+    name: string;
+    description: string;
+    html_url: string;
+}
+
+export function RepositoryList() {
+    const [repositories, setRepositories] = useState<Repository[]>([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/oopaze/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, [])
+
+    return (
+        <section className="repository-list">
+            <h1> Lista de Repositórios </h1>
+
+            <ul>
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.id} repository={repository} />
+                })}
+            </ul>
+        </section>
+    );
+}
